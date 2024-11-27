bb659f251c1c5ae2baf57f94f87c3543b50c8871
Aranfraij <aran.fraij@post.runi.ac.il>
2024-11-27 22:19:51 +0200

    Update Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,85 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+	 if (x2==0) {
+		return x1;
+	 }while (x2!= 0) {
+		x1++;
+		x2--;
+	 }
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x2 == 0) {
+			return x1;
+		}while (x2!=0) {
+			x1--;
+			x2--;
+		}
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x1==0 || x2==0) {
+			return 0;
+		}
+		int sum = x1;
+		for(int i=0; i<x2-1;i++){
+			sum = plus(sum, x1);
+		}
+		return sum;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		if (x==1 || n==0) {
+			return 1;
+		}
+		int sum = x;
+		for(int i = 0; i<n-1; i++){
+			sum = times(sum, x);
+		}
+		return sum;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x2 == 1) {
+			return x1;
+		}
+		int count=0;
+		int sum =x2;
+		while (sum<=x1) {
+			sum= plus(sum, x2);
+			count++;
+		}
+		return count;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x1<x2) {
+			return x1;
+		}
+		int sum = x1;
+		while (sum>=x2) {
+			sum = minus(sum, x2);
+		}
+		return sum;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int base=1;
+		while (pow(base, 2)<x) {
+			if (pow(plus(base, 1), 2)>x) {
+				return base;
+			}
+			base++;
+		}
+		return base;
 	}	  	  
 }
